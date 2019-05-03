5e1f6b7b6276811aef022bf365c7a9661ad56550
Edward Kim <edwardkim@Edwards-MacBook-Pro-2.local>
2019-05-03 15:11:55 -0700

    changed port to 8888

--- hashtag.py
+++ hashtag.py
@@ -117,6 +117,6 @@ def results():
         hashtags = hashtags)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8888, debug=True)
 
 
